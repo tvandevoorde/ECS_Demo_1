14b581eaf2ebb2e1e064034b199094ba2c4e9329
Tom <tom.vandevoorde@gmail.com>
2019-09-27 22:02:20 +0200

    Initial commit

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Rendering;
+using Unity.Mathematics;
+using Random = UnityEngine.Random;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private Mesh mesh;
+    [SerializeField] private Material material;
+
+    void Start()
+    {
+        EntityManager entityManager = World.Active.EntityManager;
+
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(LevelComponent),
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld),
+            typeof(MoveSpeedComponent)
+        );
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(100, Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype, entityArray);
+
+        for (int i = 0; i < entityArray.Length; i++)
+        {
+            Entity entity = entityArray[i];
+            entityManager.SetComponentData(entity, new LevelComponent { level = Random.Range(10, 20) });
+            entityManager.SetComponentData(entity, new MoveSpeedComponent { moveSpeed = -Random.Range(1, 5) });
+            entityManager.SetComponentData(entity, new Translation { Value = new float3(Random.Range(-8, 8), Random.Range(-4, 4),  0) });
+            entityManager.SetSharedComponentData(entity, new RenderMesh
+            {
+                mesh = mesh,
+                material = material
+            });
+        }
+
+        entityArray.Dispose();
+    }
+}
